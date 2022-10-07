6e071a0c01b599de599fd2d69c14a97ba12c7fe6
Victor Afanassieff <victor@afanassieff.com>
2022-10-07 20:17:45 +0200

    feat: change the bitcoin user of the bitcoin container to satoshi

--- docker/bitcoin/entrypoint.sh
+++ docker/bitcoin/entrypoint.sh
@@ -4,36 +4,36 @@ set -e
 
 # Setup user/group ids
 if [ ! -z "${PUID}" ]; then
-  if [ ! "$(id -u bitcoin)" -eq "${PUID}" ]; then
+  if [ ! "$(id -u satoshi)" -eq "${PUID}" ]; then
     
     if [ ! "${PUID}" -eq 0 ]; then
       mkdir -p /tmp/temphome
-      usermod -d /tmp/temphome bitcoin
+      usermod -d /tmp/temphome satoshi
     fi
     
     # Change the UID
-    usermod -o -u "${PUID}" bitcoin
+    usermod -o -u "${PUID}" satoshi
     
     # Cleanup the temp home dir
     if [ ! "${PUID}" -eq 0 ]; then
-      usermod -d /home bitcoin
+      usermod -d /home satoshi
       rm -Rf /tmp/temphome
     fi
   fi
 fi
 
 if [ ! -z "${PGID}" ]; then
-  if [ ! "$(id -g bitcoin)" -eq "${PGID}" ]; then
-    groupmod -o -g "${PGID}" bitcoin
+  if [ ! "$(id -g satoshi)" -eq "${PGID}" ]; then
+    groupmod -o -g "${PGID}" satoshi
   fi
 fi
 
-if [ ! '$(stat -c %u "/home")' = "$(id -u bitcoin)" ]; then
-  chown -R bitcoin:bitcoin /home
+if [ ! '$(stat -c %u "/home")' = "$(id -u satoshi)" ]; then
+  chown -R satoshi:satoshi /home
 fi
 
 if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
-  exec su-exec bitcoin "$@"
+  exec su-exec satoshi "$@"
 fi
 
 exec "$@"
