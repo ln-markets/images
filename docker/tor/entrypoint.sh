7874c73ea2053673f348f31d915111e7ba96e139
Victor Afanassieff <victor@afanassieff.com>
2022-10-07 14:13:10 +0200

    feat: add tor container

--- docker/tor/entrypoint.sh
+++ docker/tor/entrypoint.sh
@@ -2,34 +2,52 @@
 
 set -e
 
-if [ ! -z "$USER_ID" ]; then
-    usermod -u $USER_ID tor
-fi
+CONF_PATH=/etc/tor/torrc
 
-if [ ! -z "$GROUP_ID" ]; then
-    groupmod -g $GROUP_ID tor
+if [ ! -z "${PUID}" ]; then
+  if [ ! "$(id -u toruser)" -eq "${PUID}" ]; then
+    
+    if [ ! "${PUID}" -eq 0 ]; then
+      mkdir -p /tmp/temphome
+      usermod -d /tmp/temphome toruser
+    fi
+    
+    # Change the UID
+    usermod -o -u "${PUID}" toruser
+    
+    # Cleanup the temp home dir
+    if [ ! "${PUID}" -eq 0 ]; then
+      usermod -d /home toruser
+      rm -Rf /tmp/temphome
+    fi
+  fi
 fi
 
 if [ $1 = "tor" ];then
-    if [ ! "$USE_CONF" ]; then
-        rm -f $CONF_PATH
+    if [ ! "$ENV_CONFIG" ]; then
+      echo "Deleting default torrc"
+      rm -f $CONF_PATH
+      
+      if [ ! -z "$PASSWORD" ]; then
+          echo HashedControlPassword $(tor --hash-password "${PASSWORD}" | tail -n 1) >> $CONF_PATH
+      fi
 
-        if [ ! -z "$PASSWORD" ]; then
-            echo HashedControlPassword $(tor --hash-password "${PASSWORD}" | tail -n 1) >> /etc/tor/torrc
-        fi
-
-        for tor_options in $(printenv | grep TOR_)
-        do
-            options="${tor_options:4}"
-            readarray -d = -t options <<< "$options"
-            option=${options[0]}
-            value=${options[1]}
-            echo $option $value >> $CONF_PATH
-        done
+      for tor_options in $(printenv | grep TOR_)
+      do
+          options="${tor_options:4}"
+          readarray -d = -t options <<< "$options"
+          option=${options[0]}
+          value=${options[1]}
+          echo $option $value >> $CONF_PATH
+      done
     fi
 
-    su-exec tor tor -f $CONF_PATH --verify-config
-    exec su-exec tor tor -f $CONF_PATH
+    if test -f "$CONF_PATH"; then
+      su-exec toruser tor -f $CONF_PATH --verify-config
+      exec su-exec toruser tor -f $CONF_PATH
+    fi
+
+    exec su-exec toruser tor
 else
     exec "$@"
 fi
