8dd775289b28e35b8a85ddcaef0fc7304468a470
Victor Afanassieff <victor@afanassieff.com>
2022-10-08 21:45:15 +0200

    Update regtest user acording to new bitcoin images

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LN Markets Images
 
-All the Bitcoin and lightning images used by [LN Markets](https://alpinelinux.org/)
+All the Bitcoin and lightning images used by [LN Markets](https://lnmarkets.com)
 
 ## Introduction
 
@@ -9,6 +9,10 @@ All images built are based on [Alpine Linux](https://alpinelinux.org/)
 Every image run the app as a non root user, you can also set the user id and group id who will run the image and map it to your needs.
 
 ## Configuration
+
+All the images create a user with default user id of `1000` and default group id of `1000`
+
+All default user home is set to `/home` to ease the use of volumes and mapping
 
 `PUID` and `PGID` are common to all images
 
@@ -19,6 +23,23 @@ By changing them variables you will also change the owner on the app files
 | PUID | Set the user id running the app       |
 | PGID | Set the user group id running the app |
 
-All the images create a user with default user id of _1000_ and default group id of _1000_
+## Images
 
-All default user home is set to `/home` to ease the use of volumes and mapping
+### Bitcoin
+
+### Bitcoin Regtest
+
+### Taro
+
+### LND
+
+### Lightning Terminal
+
+### Lndmon
+
+### Loop
+
+### Pool
+
+### Tor
+### Faraday
--- docker/bitcoin-regtest/regtest.sh
+++ docker/bitcoin-regtest/regtest.sh
@@ -7,15 +7,7 @@ BITCOIN_CONF_FILE=/home/.bitcoin/bitcoin.conf
 mkdir -p /home/.bitcoin/
 touch ${BITCOIN_CONF_FILE}
 
-cat > /home/mine.sh << 'EOF'
-#! /usr/bin/env bash
-
-if [ -a /home/satoshi ]; then
-    bitcoin-cli -regtest generatetoaddress 1 $(cat /home/satoshi)
-fi
-EOF
-
-cat >${BITCOIN_CONF_FILE} << 'EOF'
+cat > ${BITCOIN_CONF_FILE} << 'EOF'
 server=1
 regtest=1
 txindex=1
@@ -31,8 +23,8 @@ fallbackfee=0.0002
 rpcbind=0.0.0.0
 rpcallowip=0.0.0.0/0
 rpcport=18443
-rpcuser=bitcoin
-rpcpassword=bitcoin
+rpcuser=satoshi
+rpcpassword=satoshi
 EOF
 
 cat > /home/crontab << 'EOF'
@@ -40,11 +32,19 @@ cat > /home/crontab << 'EOF'
 * * * * * sleep 30 && /home/mine.sh
 EOF
 
+cat > /home/mine.sh << 'EOF'
+#! /usr/bin/env bash
+
+if [ -a /home/satoshi ]; then
+    bitcoin-cli -regtest generatetoaddress 1 $(cat /home/address)
+fi
+EOF
+
 chmod +x /home/mine.sh
-chown -R bitcoin:bitcoin /home
+chown -R satoshi:satoshi /home
 
 # Must be run as root
-crontab -u bitcoin /home/crontab
+crontab -u satoshi /home/crontab
 crond -b
 
 # Create the bitcoin wallet if needed and load it in background
@@ -53,19 +53,19 @@ create_and_load_wallet() {
     sleep 2
     if [ ! -a /home/.bitcoin/regtest/wallets/satoshi/wallet.dat ]; then
         echo 'Create default wallet'
-        su-exec bitcoin bash -c "bitcoin-cli -regtest createwallet satoshi"
+        su-exec satoshi bash -c "bitcoin-cli -regtest createwallet satoshi"
     fi
 
-    su-exec bitcoin bash -c "bitcoin-cli -regtest loadwallet satoshi 2>/dev/null" | true
+    su-exec satoshi bash -c "bitcoin-cli -regtest loadwallet satoshi 2>/dev/null" | true
 
     # Create one address for the wallet where all coins will be mined
-    if [ ! -a /home/satoshi ]; then
-        echo 'Create satoshi address'
-        su-exec bitcoin bash -c "bitcoin-cli -regtest getnewaddress > /home/satoshi"
+    if [ ! -a /home/address ]; then
+        echo 'Create address'
+        su-exec satoshi bash -c "bitcoin-cli -regtest getnewaddress > /home/address"
     fi
 }
 
 # Run create_and_load_wallet in background 
 create_and_load_wallet &
 
-su-exec bitcoin bitcoind
+su-exec satoshi bitcoind
