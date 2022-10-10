cae6f64ee971f8aaccbad93c4078968db702088f
Victor Afanassieff <victor@afanassieff.com>
2022-10-10 12:13:53 +0200

    feat: update entrypoint for faraday

--- docker/faraday/entrypoint.sh
+++ docker/faraday/entrypoint.sh
@@ -2,34 +2,7 @@
 
 set -e
 
-if [ ! -z "${PUID}" ]; then
-  if [ ! "$(id -u faraday)" -eq "${PUID}" ]; then
-    
-    if [ ! "${PUID}" -eq 0 ]; then
-      mkdir -p /tmp/temphome
-      usermod -d /tmp/temphome faraday
-    fi
-    
-    # Change the UID
-    usermod -o -u "${PUID}" faraday
-    
-    # Cleanup the temp home dir
-    if [ ! "${PUID}" -eq 0 ]; then
-      usermod -d /home faraday
-      rm -Rf /tmp/temphome
-    fi
-  fi
-fi
-
-if [ ! -z "${PGID}" ]; then
-  if [ ! "$(id -g faraday)" -eq "${PGID}" ]; then
-    groupmod -o -g "${PGID}" faraday
-  fi
-fi
-
-if [ ! '$(stat -c %u "/home")' = "$(id -u faraday)" ]; then
-  chown -R faraday:faraday /home
-fi
+/docker/update-user.sh
 
 if [ "$1" = "faraday" ] || [ "$1" = "frcli" ]; then
   exec su-exec faraday "$@"
---
+++ scripts/update-user.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -z "${PUID}" ]; then
+  if [ $(id -u ${USER}) -ne $PUID ]; then
+    echo "Changing ${USER} user id to ${PUID}"
+    
+    if [ ! "${PUID}" -eq 0 ]; then
+      mkdir -p /tmp/temphome
+      usermod -d /tmp/temphome ${USER}
+    fi
+    
+    # Change the UID
+    usermod -o -u "${PUID}" ${USER}
+    
+    # Cleanup the temp home dir
+    if [ ! "${PUID}" -eq 0 ]; then
+      usermod -d /home ${USER}
+      rm -Rf /tmp/temphome
+    fi
+  fi
+fi
+
+if [ ! -z "${PGID}" ]; then
+  if [ $(id -g ${USER}) -ne $PGID ]; then
+    echo "Changing ${USER} group to ${PGID}"
+    groupmod -o -g "${PGID}" ${USER}
+  fi
+fi
+
+if [ $(stat -c %u "/home") -ne $(id -u ${USER}) ]; then
+  echo "Updating permissions on /home and recursivly on ${DATA_PATH}"
+  chown ${USER}:${USER} /home
+
+  if [ ! -d "$DATA_PATH" ]; then
+    mkdir -p $DATA_PATH
+  fi
+
+  chown -R ${USER}:${USER} ${DATA_PATH}
+fi
